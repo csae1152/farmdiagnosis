109ae6e44a633899070e844f6c68fdd9f0cbabb8
Helmut Steiner <helmut.steinersti2@gmail.com>
2020-05-23 09:04:07 +0200

    Scala compiler build.

--- src/main/java/com/agricol/main/HtmlEchoServer.java
+++ src/main/java/com/agricol/main/HtmlEchoServer.java
@@ -4,13 +4,18 @@ import io.vertx.core.AbstractVerticle;
 
 /**
  * @author Helmut Steiner
+ * @see AbstractVerticle
  */
 public class HtmlEchoServer extends AbstractVerticle {
+    /**
+     * Starts the main verticle
+     *
+     * @throws Exception
+     */
     @Override
     public void start() throws Exception {
         vertx.createHttpServer().requestHandler(req -> req.response().end("farmdiagnosis"))
                 .listen(
                         Integer.getInteger("http.port"), System.getProperty("http.address", "0.0.0.0"));
-
     }
 }
